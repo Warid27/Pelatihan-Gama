22c4fd76722eb474bb238483e21f6068f1fbd754
Warid27 <mohamadsinatr3@gmail.com>
2024-12-18 21:53:18 +0700

    Create and Read + get data from API and ngrox

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -4,6 +4,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -20,6 +21,44 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+// CORS Configuration
+const allowedOrigins = [
+  "http://localhost:8888",
+  "https://camel-devoted-teal.ngrok-free.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+
+      // Check if the origin is in the allowed list
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      // If not, reject the request
+      return callback(new Error("Not allowed by CORS"));
+    },
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "x-api-key",
+      "x-payload",
+      "ngrok-skip-browser-warning", // Add the custom header here
+    ],
+    credentials: true,
+    maxAge: 86400,
+  })
+);
+
+// Preflight Request Handler
+app.options("*", (req, res) => {
+  res.status(204).end(); // Responds to OPTIONS request with no content
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
